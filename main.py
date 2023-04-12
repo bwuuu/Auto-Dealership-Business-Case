37bc9eff591f6140abb8e4d102ddf62dbb4c0456
bwuuu <117622597+bwuuu@users.noreply.github.com>
2023-04-12 19:18:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ import numpy as np
 import pandas as pd
 
 st.title("Sales Volume Assumption")
-nc_sales_y1 = st.number_input("1st Year NC Sales Volume")
-nc_sales_y2 = st.number_input("2nd Year NC Sales Volume")
-nc_sales_y3 = st.number_input("3rd Year NC Sales Volume")
-nc_sales_y4 = st.number_input("4th Year NC Sales Volume")
-nc_sales_y5 = st.number_input("5th Year NC Sales Volume")
+col1, col2, col3, col4, col5 = st.columns(5)
 
-uc_sales_y1 = st.number_input("1st Year UC Sales Volume")
-uc_sales_y2 = st.number_input("2nd Year UC Sales Volume")
-uc_sales_y3 = st.number_input("3rd Year UC Sales Volume")
-uc_sales_y4 = st.number_input("4th Year UC Sales Volume")
-uc_sales_y5 = st.number_input("5th Year UC Sales Volume")
+with col1:
+    nc_sales_y1 = st.number_input("1st Year NC Sales Volume")
+    uc_sales_y1 = st.number_input("1st Year UC Sales Volume")
+with col2:
+    nc_sales_y2 = st.number_input("2nd Year NC Sales Volume")
+    uc_sales_y2 = st.number_input("2nd Year UC Sales Volume")
+with col3:
+    nc_sales_y3 = st.number_input("3rd Year NC Sales Volume")
+    uc_sales_y3 = st.number_input("3rd Year UC Sales Volume")
+with col4:
+    nc_sales_y4 = st.number_input("4th Year NC Sales Volume")
+    uc_sales_y4 = st.number_input("4th Year UC Sales Volume")
+with col5:
+    nc_sales_y5 = st.number_input("5th Year NC Sales Volume")
+    uc_sales_y5 = st.number_input("5th Year UC Sales Volume")
+
