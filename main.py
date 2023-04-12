b2e4a45bdb517615c23f3b219eac7a11fbc7448f
bwuuu <117622597+bwuuu@users.noreply.github.com>
2023-04-12 19:24:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,18 +6,19 @@ st.title("Sales Volume Assumption")
 col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
-    nc_sales_y1 = st.number_input("1st Year NC Sales Volume", min_value=(int))
-    uc_sales_y1 = st.number_input("1st Year UC Sales Volume", min_value=(int))
+    nc_sales_y1 = st.number_input("1st Year NC Sales Volume", format=(%i))
+    uc_sales_y1 = st.number_input("1st Year UC Sales Volume")
 with col2:
-    nc_sales_y2 = st.number_input("2nd Year NC Sales Volume", min_value=(int))
-    uc_sales_y2 = st.number_input("2nd Year UC Sales Volume", min_value=(int))
+    nc_sales_y2 = st.number_input("2nd Year NC Sales Volume")
+    uc_sales_y2 = st.number_input("2nd Year UC Sales Volume")
 with col3:
-    nc_sales_y3 = st.number_input("3rd Year NC Sales Volume", min_value=(int))
-    uc_sales_y3 = st.number_input("3rd Year UC Sales Volume", min_value=(int))
+    nc_sales_y3 = st.number_input("3rd Year NC Sales Volume")
+    uc_sales_y3 = st.number_input("3rd Year UC Sales Volume")
 with col4:
-    nc_sales_y4 = st.number_input("4th Year NC Sales Volume", min_value=(int))
-    uc_sales_y4 = st.number_input("4th Year UC Sales Volume", min_value=(int))
+    nc_sales_y4 = st.number_input("4th Year NC Sales Volume")
+    uc_sales_y4 = st.number_input("4th Year UC Sales Volume")
 with col5:
-    nc_sales_y5 = st.number_input("5th Year NC Sales Volume", min_value=(int))
-    uc_sales_y5 = st.number_input("5th Year UC Sales Volume", min_value=(int))
+    nc_sales_y5 = st.number_input("5th Year NC Sales Volume")
+    uc_sales_y5 = st.number_input("5th Year UC Sales Volume")
+
 
